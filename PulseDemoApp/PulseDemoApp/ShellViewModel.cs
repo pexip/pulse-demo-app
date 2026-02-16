d2f1bf6265c8f60d7ac4168b04946389b3ab24c4
paul.enascut <paul.enascut@pexip.com>
2026-02-16 15:12:55 +0200

    PulseDemoApp: Add Registration card

--- PulseDemoApp/PulseDemoApp/ShellViewModel.cs
+++ PulseDemoApp/PulseDemoApp/ShellViewModel.cs
@@ -1,5 +1,48 @@
-﻿namespace PulseDemoApp;
+﻿using CommunityToolkit.Mvvm.Input;
+using PulseDemoApp.Validation;
 
-public class ShellViewModel
+namespace PulseDemoApp;
+
+public partial class ShellViewModel : ObservableObject
 {
+    #region Properties
+
+    [ObservableProperty]
+    private bool joinCardEnabled;
+
+    [ObservableProperty]
+    private bool hostPinCardEnabled;
+
+    [ObservableProperty]
+    private bool conferenceCardEnabled;
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(RegisterCommand))]
+    private string videoAddress;
+
+    #endregion
+
+    public ShellViewModel()
+    {
+        this.videoAddress = string.Empty;
+    }
+
+    #region Commands
+
+    [RelayCommand(CanExecute = nameof(CanRegister))]
+    private async Task RegisterAsync()
+    {
+        
+    }
+
+    #endregion
+
+    #region Command Validations
+
+    private bool CanRegister()
+    {
+        return new AliasValidator().ValidateFullyQualifiedAlias(VideoAddress);
+    }
+
+    #endregion
 }
---
+++ PulseDemoApp/PulseDemoApp/Validation/AliasValidator.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="AliasValidator.cs" company="Pexip">
+// Copyright (c) Pexip. All rights reserved.
+// </copyright>
+
+namespace PulseDemoApp.Validation;
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+// Refer to https://docs.pexip.com/admin/about_aliases.htm
+public sealed class AliasValidator
+{
+    // FQDN provided
+    public bool ValidateFullyQualifiedAlias(string alias)
+    {
+        var context = new ValidationContext(alias);
+        var attributes = new List<ValidationAttribute>
+        {
+            new RequiredAttribute(),
+            new EmailAddressAttribute(),
+        };
+
+        var results = new List<ValidationResult>();
+        bool qualified = Validator.TryValidateValue(alias, context, results, attributes);
+
+        return qualified;
+    }
+
+    // FQDN inferred
+    public bool ValidateRegisteredAlias(string address)
+    {
+        var context = new ValidationContext(address);
+        var attributes = new List<ValidationAttribute>
+        {
+            new RequiredAttribute(),
+            new RegularExpressionAttribute(@"^[\p{L}\p{N}_@.-]*$"), // Match Unicode letters, numbers, dashes, underscore at signs and periods
+        };
+
+        var results = new List<ValidationResult>();
+        bool qualified = Validator.TryValidateValue(address, context, results, attributes);
+
+        return qualified;
+    }
+}
