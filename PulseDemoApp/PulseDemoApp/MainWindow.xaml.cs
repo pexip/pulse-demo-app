9e77e23918932c5149c17b1c6425624248cb03e3
Sunjay Kalsi <sunjay.kalsi@pexip.com>
2026-02-16 12:09:06 +0000

    Set Pexip App Icon

--- PulseDemoApp/PulseDemoApp/MainWindow.xaml.cs
+++ PulseDemoApp/PulseDemoApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 using Microsoft.UI.Xaml;
+using System;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -13,6 +14,12 @@ namespace PulseDemoApp
         public MainWindow()
         {
             InitializeComponent();
+
+            // Set window icon
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hwnd);
+            var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            appWindow.SetIcon("Assets\\pexip.ico");
         }
     }
 }
