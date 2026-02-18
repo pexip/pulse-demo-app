efb19b503c18d5cf2d5df003740b837fa1592d0b
Sunjay Kalsi <sunjay.kalsi@pexip.com>
2026-02-18 11:39:12 +0000

    Add the Join UI and Dummy data (#3)
    Added UI elements for the Join page and the Observable Device Object

---
+++ PulseDemoApp/PulseDemoApp/Devices/MediaDevice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PulseDemoApp.Devices
+{
+    public class MediaDevice : ObservableObject
+    {
+        public MediaDevice(uint uid, string name, bool isDefault, bool isConnected)
+        {
+            Uid = uid;
+            Name = name;
+            IsDefault = isDefault;
+            IsConnected = isConnected;
+        }
+
+        public uint Uid { get; }
+
+        public string Name { get; }
+
+        public bool IsDefault { get; }
+
+        public bool IsConnected { get; }
+    }
+}
