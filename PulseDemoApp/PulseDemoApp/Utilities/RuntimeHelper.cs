6c48b3bf560f7267263c652a60b659b0c5bc135f
paul.enascut <paul.enascut@pexip.com>
2026-03-13 13:48:21 +0200

    PulseDemoApp: Add EULA

---
+++ PulseDemoApp/PulseDemoApp/Utilities/RuntimeHelper.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="RuntimeHelper.cs" company="Pexip">
+// Copyright (c) Pexip. All rights reserved.
+// </copyright>
+
+namespace PulseDemoApp.Utilities;
+
+using System.Runtime.InteropServices;
+
+public static class RuntimeHelper
+{
+    public static bool IsMSIX
+    {
+        get
+        {
+            int length = 0;
+            return NativeMethods.GetCurrentPackageFullName(ref length, null) != 15700L;
+        }
+    }
+
+    private static class NativeMethods
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        [DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+        internal static extern int GetCurrentPackageFullName(ref int packageFullNameLength, [Out] char[]? packageFullName);
+    }
+}
